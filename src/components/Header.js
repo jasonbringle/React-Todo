453a150eee80da793c57c28f276a396442437c2f
Jason <Jason@MacBook-Pro-3.local>
2020-05-06 19:15:39 -0700

    built out some state and components

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Header = ()=>{
+    return <p>TODOS</p>
+}
+export default Header;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,12 @@
+import React from 'react'
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+const TodoList = props => {
+    return <div>
+        {props.initialTodos.map(todo => {
+            <Todo todo={todo} />
+            })
+        }</div>
+
+}
+export default TodoList;
